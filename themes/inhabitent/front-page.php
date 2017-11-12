7f37b2c42609ae1a4834c591273be7ff5c1fcb36
Jordan <jordplamondon@gmail.com>
2017-11-11 21:16:15 -0800

    add stylesheets, footer & hero img

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -11,6 +11,9 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<section class="hero">
+				<img src="<?php echo get_template_directory_uri() . '/assets/project-04/images/logos/inhabitent-logo-full.svg'; ?>" alt="Inhabitent Logo" />
+			</section>
 
 		<?php if ( have_posts() ) : ?>
 
