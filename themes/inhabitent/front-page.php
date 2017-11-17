7aa11769cb4312c2ef20c7107f780d7db0f86557
Jordan <jordplamondon@gmail.com>
2017-11-16 16:47:47 -0800

    style front page, journal. Remove sidebar widget from specific pages

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -8,22 +8,19 @@
 get_header(); ?>
 
 
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<section class="hero">
 				<img src="<?php echo get_template_directory_uri() . '/assets/project-04/images/logos/inhabitent-logo-full.svg'; ?>" alt="Inhabitent Logo" />
 			</section>
 
-		<section class="product-info container">
+		<section class="product-info-container">
             <h2>Shop Stuff</h2>
             <?php
                $terms = get_terms( array(
-                   'taxonomy' => 'product-type',
+                   'taxonomy' 	=> 'product-type',
                    'hide_empty' => 0,
 							 ) );
-							 
-							 d($terms);
 
                if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
             ?>
@@ -32,7 +29,7 @@ get_header(); ?>
                   <?php foreach ( $terms as $term ) : ?>
 
                      <div class="product-type-block-wrapper">
-                        <img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+                        <img src="<?php echo get_template_directory_uri() . '/assets/project-04/images/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
                         <p><?php echo $term->description; ?></p>
                         <p><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
                      </div>
