dc393eb5761c611d691205d131f81a779a81eb05
Jordan <jordplamondon@gmail.com>
2017-11-28 23:04:03 -0800

    remove unnecessary html from archive product, continue to style shop page

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -59,13 +59,12 @@ get_header(); ?>
         
         <?php endif; ?>
 
-		    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-    
+		    <!-- <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>-->
       </header><!-- .page-header -->
 
 	
 
-			  <?php the_posts_navigation(); ?>
+			  <!-- <?php the_posts_navigation(); ?> -->
 
       <?php else : ?>
     
