c035e92fee9d3a25f39f12b46df8dc64d54ff47c
Jordan <jordplamondon@gmail.com>
2017-11-30 14:42:48 -0800

    attempt to fix search feature

--- themes/inhabitent/js/scripts.js
+++ themes/inhabitent/js/scripts.js
@@ -4,19 +4,19 @@
 
  $( "#search-submit" ).on('click', function(e) {
    e.preventDefault();
-   $( "#search-field" ).toggle().focus();
+   $( ".search-field" ).toggle().focus();
    $(document).keypress(function(){
       if(event.which == 13){
         
-      if($("#search-field").val() != ''){
+      if($(".search-field").val() != ''){
         $( ".search-form" ).submit();
       }
          
     }
    });
  });
- $( "#search-field" ).on('blur', function(){
-   $( "#search-field" ).toggle();
+ $( ".search-field" ).on('blur', function(){
+   $( ".search-field" ).toggle();
  });
  })(jQuery);
 
